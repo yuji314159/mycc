ef25d7863c6838172be89a2fd7c3995a1f8a587c
yuji314159 <yuji314159@gmail.com>
2020-04-09 23:56:48 +0900

    トークナイザを導入

--- mycc.c
+++ mycc.c
@@ -1,5 +1,100 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+    TK_RESERVED,
+    TK_NUM,
+    TK_EOF,
+} TokenType;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenType type;
+    Token *next;
+    int val;
+    char *str;
+};
+
+// グローバル変数嫌い
+Token *token;
+
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+bool consume(char op) {
+    if (token->type != TK_RESERVED || token->str[0] != op) {
+        return false;
+    }
+    token = token->next;
+    return true;
+}
+
+void expect(char op) {
+    if (token->type != TK_RESERVED || token->str[0] != op) {
+        error_at(token->str, "'%c' ではありません", op);
+    }
+    token = token->next;
+}
+
+int expect_number() {
+    if (token->type != TK_NUM) {
+        error_at(token->str, "数ではありません");
+    }
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof() {
+    return token->type == TK_EOF;
+}
+
+Token *new_token(TokenType type, Token *cur, char *str) {
+    Token *tok = calloc(1, sizeof(Token));
+    tok->type = type;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+Token *tokenize(char *p) {
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        if (isspace(*p)) {
+            ++p;
+        } else if (*p == '+' || *p == '-') {
+            cur = new_token(TK_RESERVED, cur, p++);
+        } else if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+        } else {
+            error_at(token->str, "トークナイズできません");
+        }
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -7,26 +102,21 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    char *p = argv[1];
+    user_input = argv[1];
+    token = tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("    mov rax, %ld\n", strtol(p, &p, 10));
 
-    while (*p) {
-        if (*p == '+') {
-            ++p;
-            printf("    add rax, %ld\n", strtol(p, &p, 10));
-        } else if (*p == '-') {
-            ++p;
-            printf("    sub rax, %ld\n", strtol(p, &p, 10));
-        } else {
-            fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-            return 1;
+    printf("    mov rax, %d\n", expect_number());
+    while (!at_eof()) {
+        if (consume('+')) {
+            printf("    add rax, %d\n", expect_number());
+        } else if (consume('-')) {
+            printf("    sub rax, %d\n", expect_number());
         }
     }
-
     printf("    ret\n");
 
     return 0;
