274baf211eca5f12ce81bbe3fd3bc54ebaa80984
yuji314159 <yuji314159@gmail.com>
2020-04-11 02:24:23 +0900

    AST/スタックマシンの導入

--- mycc.c
+++ mycc.c
@@ -12,7 +12,6 @@ typedef enum {
 } TokenType;
 
 typedef struct Token Token;
-
 struct Token {
     TokenType type;
     Token *next;
@@ -82,7 +81,7 @@ Token *tokenize(char *p) {
     while (*p) {
         if (isspace(*p)) {
             ++p;
-        } else if (*p == '+' || *p == '-') {
+        } else if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
             cur = new_token(TK_RESERVED, cur, p++);
         } else if (isdigit(*p)) {
             cur = new_token(TK_NUM, cur, p);
@@ -96,6 +95,108 @@ Token *tokenize(char *p) {
     return head.next;
 }
 
+typedef enum {
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+} NodeType;
+
+typedef struct Node Node;
+struct Node {
+    NodeType type;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+Node *new_node(NodeType type, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->type = type;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->type = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr();
+
+Node *primary() {
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_node_num(expect_number());
+}
+
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+void gen(Node *node) {
+    if (node->type == ND_NUM) {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch (node->type) {
+        case ND_ADD:
+            printf("    add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("    sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("    imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("    cqo\n");
+            printf("    idiv rdi\n");
+            break;
+    }
+
+    printf("    push rax\n");
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の数が正しくありません\n");
@@ -104,19 +205,15 @@ int main(int argc, char **argv) {
 
     user_input = argv[1];
     token = tokenize(argv[1]);
+    Node *node = expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    printf("    mov rax, %d\n", expect_number());
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("    add rax, %d\n", expect_number());
-        } else if (consume('-')) {
-            printf("    sub rax, %d\n", expect_number());
-        }
-    }
+    gen(node);
+
+    printf("    pop rax\n");
     printf("    ret\n");
 
     return 0;
